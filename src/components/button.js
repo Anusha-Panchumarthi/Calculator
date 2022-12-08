cef5fb281d1e36b14d20c9f093fc7507d411e4d5
Anusha-Panchumarthi <anusha.panchumarthi@gmail.com>
2022-12-08 13:41:46 +0530

    fixed bug

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button({ val, handleClick }){
     return(
-        <button className="btn" name={val} onClick={handleClick}>
+        <button className="btn" id={val} onClick={handleClick}>
             {val}
         </button>
     )
